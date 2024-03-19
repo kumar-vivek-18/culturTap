6cf9f0b129744b4df3b30c924709544ae2605c1b
Vivek Kumar <vivek@culturtap.com>
2024-03-19 19:37:26 +0530

    modified

--- frontend/src/components/about/ServicesModal.js
+++ frontend/src/components/about/ServicesModal.js
@@ -9,10 +9,10 @@ const ServicesModal = ({ isOpen, onClose, service }) => {
             <div className='max-sm:w-[380px] max-sm:h-[620px] w-[629px] h-[570px] overflow-hidden rounded-2xl'>
                 <div className='relative'>
                     <img src={service.modalLink} alt="service" className='max-sm:scale-125 max-sm:-translate-y-9   max-sm:w-[380px] max-sm:z-100 w-[629px] h-[280px] scale-105' />
-                    <p className='text-center font-bold text-[24px] mt-3'>{service.title}</p>
-                    <p className='w-4/5 text-[16px] mx-auto text-center mt-2'>{service.desc}</p>
-                    <p className='font-semibold text-center w-4/5 mx-auto text-[16px]'>{service.extra}</p>
-                    <p className='font-bold text-[16px] text-[#2e2c43] text-center mt-3 '>Download App Now</p>
+                    <p className='text-center font-bold text-[22px] mt-3'>{service.title}</p>
+                    <p className='w-4/5 text-[14px] mx-auto text-center mt-2'>{service.desc}</p>
+                    <p className='font-semibold text-center w-4/5 mx-auto text-[14px]'>{service.extra}</p>
+                    <p className='font-bold text-[14px] text-[#2e2c43] text-center mt-3 '>Download App Now</p>
                     <div className='flex justify-center sticky '>
                         <img src={appStore} alt="app" />
                         <img src={playStore} alt="app" />
