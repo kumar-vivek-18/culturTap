d8b6f8ae30624f5e09211052d70d65352adffbe6
Vivek Kumar <vivek@culturtap.com>
2024-03-19 19:39:14 +0530

    modified

--- frontend/src/components/about/ServicesModal.js
+++ frontend/src/components/about/ServicesModal.js
@@ -6,7 +6,7 @@ const ServicesModal = ({ isOpen, onClose, service }) => {
     // console.log("service", service);
     return (
         <Modal isOpen={isOpen} onClose={onClose}>
-            <div className='max-sm:w-[380px] max-sm:h-[620px] w-[629px] h-[570px] overflow-hidden rounded-2xl'>
+            <div className='max-sm:w-[380px] max-sm:h-[600px] w-[629px] h-[570px] overflow-hidden rounded-2xl'>
                 <div className='relative'>
                     <img src={service.modalLink} alt="service" className='max-sm:scale-125 max-sm:-translate-y-9   max-sm:w-[380px] max-sm:z-100 w-[629px] h-[280px] scale-105' />
                     <p className='text-center font-bold text-[22px] mt-3'>{service.title}</p>
@@ -14,8 +14,8 @@ const ServicesModal = ({ isOpen, onClose, service }) => {
                     <p className='font-semibold text-center w-4/5 mx-auto text-[14px]'>{service.extra}</p>
                     <p className='font-bold text-[14px] text-[#2e2c43] text-center mt-3 '>Download App Now</p>
                     <div className='flex justify-center sticky '>
-                        <img src={appStore} alt="app" />
-                        <img src={playStore} alt="app" />
+                        <img src={appStore} alt="app" className='scale-90' />
+                        <img src={playStore} alt="app" className='scale-90' />
                     </div>
                 </div>
             </div>
