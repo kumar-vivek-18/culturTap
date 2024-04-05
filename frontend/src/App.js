945c356183f013d3722baf4a465829e82d2d32b4
Vivek Kumar <vivek@culturtap.com>
2024-04-05 17:10:40 +0530

    modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,20 +11,20 @@ import Reports from './components/contacts/Reports';
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
 
       <Routes>
         <Route exact path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/services' element={<About />} />
-        <Route path='/earn' element={<Earn />} />
-        <Route path='/contact' element={<Contacts />} />
-        <Route path='/contact/report' element={<Reports />} />
+        <Route exact path='/about' element={<About />} />
+        <Route exact path='/services' element={<About />} />
+        <Route exact path='/earn' element={<Earn />} />
+        <Route exact path='/contact' element={<Contacts />} />
+        <Route exact path='/contact/report' element={<Reports />} />
       </Routes>
 
       <Footer />
-    </div>
+    </>
   );
 }
 
